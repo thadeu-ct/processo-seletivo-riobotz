b7cb9f7332d659a16362afe1a1ef8ba21fa7f0f6
joaocapechi <joaopedrocapechi@gmail.com>
2026-03-14 10:50:44 -0300

    Consertando o JSON do login

--- api/api.py
+++ api/api.py
@@ -118,7 +118,7 @@ def login():
     
     return {
         "nome": row["nome"],
-        "mat": row["matricula"],
+        "matricula": row["matricula"],
         "botcoin": row["botcoin"]
     }
 

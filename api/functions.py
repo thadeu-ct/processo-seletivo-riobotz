71d92bccf2a5e50f33c716feba6c010b692ff888
joaocapechi <126024484+joaocapechi@users.noreply.github.com>
2026-03-17 10:53:03 -0300

    Update functions.py

--- api/functions.py
+++ api/functions.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+import psycopg2
 
 '''
     Constantes
@@ -16,6 +17,12 @@ ERRO_SENHA = 5
 '''
     Funções auxiliares
 '''
+def get_db_connection():
+    # O psycopg2 entende perfeitamente a URL do Supabase
+    conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+    return conn
+
+
 def comecou_processo(data: datetime):
     return data.date() >= DATA_INICIO.date()
 
@@ -36,3 +43,31 @@ def create_hash(texto: str):
 
 def compare_hash(texto: str, hash: str):
     return check_password_hash(hash, texto)
+
+
+def get_user(matriucla: str):
+    try:
+        banco = get_db_connection()
+        db = banco.cursor(psycopg2.extras.RealDictCursor)
+
+        db.execute(
+            "SELECT * FROM users WHERE matricula = %s"
+            (matricula,)
+        )
+
+        rows = db.fetchmany(1)
+
+        db.close()
+        banco.close()
+    except Exception as e:
+        print("Erro no banco: {e}")
+        return {
+            "erro": str(e)
+        }, 500
+    
+    if len(rows) != 0:
+        return {
+            "erro": "matricula inválida"
+        }
+    
+    return rows[0]

873cad9cff18b5fa5944215ee9c54de7924e1ed6
joaocapechi <joaopedrocapechi@gmail.com>
2026-04-09 14:09:19 -0300

    Teste de debug

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -52,7 +52,11 @@ function Quiz() {
           );
         }
 
-        const resQuiz = await fetch(`${API_URL}/quiz/get?id=${id}&qtd=5`);
+        const resQuiz = await fetch(`${API_URL}/quiz/get`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ id, qtd: 5 }),
+        });
         const data = await resQuiz.json();
 
         if (Array.isArray(data) && data.length > 0) {
